8937488496ea09a7949f1854f179635dfb6e3c3f
pronesto <pronesto@gmail.com>
2026-01-15 16:30:23 +0000

    Adding new loop expansion pass.

--- lib/Passes/LoopExpansionPass.cpp
+++ lib/Passes/LoopExpansionPass.cpp
@@ -16,51 +16,62 @@ static std::optional<int64_t> getConstantIndex(Value v) {
   return std::nullopt;
 }
 
-static LogicalResult expandForLoop(scf::ForOp forOp) {
+static LogicalResult expandForLoop(scf::ForOp forOp, RewriterBase &rewriter) {
   auto lbOpt = getConstantIndex(forOp.getLowerBound());
   auto ubOpt = getConstantIndex(forOp.getUpperBound());
   auto stepOpt = getConstantIndex(forOp.getStep());
 
-  if (!lbOpt || !ubOpt || !stepOpt)
+  if (!lbOpt || !ubOpt || !stepOpt || *stepOpt <= 0)
     return failure();
 
   int64_t lb = *lbOpt;
   int64_t ub = *ubOpt;
   int64_t step = *stepOpt;
-
-  if (step <= 0)
-    return failure();
-
   int64_t tripCount = (ub - lb + step - 1) / step;
-  if (tripCount <= 0)
-    return success(); // empty loop
 
-  OpBuilder builder(forOp);
+  Location loc = forOp.getLoc();
+  IRMapping mapping;
 
-  Block *parentBlock = forOp->getBlock();
-  auto insertionPoint = Block::iterator(forOp);
+  // 1. Initialize mapping for iter_args
+  // The initial values of the loop's block arguments come from the init_args
+  for (auto [arg, initVal] :
+      llvm::zip(forOp.getRegionIterArgs(), forOp.getInits())) {
+    mapping.map(arg, initVal);
+  }
 
-  Value iv = forOp.getInductionVar();
-
-  // Insert before the loop
-  builder.setInsertionPoint(parentBlock, insertionPoint);
+  rewriter.setInsertionPoint(forOp);
 
   for (int64_t i = 0; i < tripCount; ++i) {
-    IRMapping mapping;
-
-    // Create constant IV value
+    // Update the Induction Variable for this iteration
     Value ivValue =
-      arith::ConstantIndexOp::create(builder, forOp.getLoc(), lb + i * step);
+      arith::ConstantIndexOp::create(rewriter, loc, lb + i * step);
+    mapping.map(forOp.getInductionVar(), ivValue);
 
-    mapping.map(iv, ivValue);
-
-    // Clone body operations except scf.yield
+    // Clone the body
     for (Operation &op : forOp.getBody()->without_terminator()) {
-      builder.clone(op, mapping);
+      rewriter.clone(op, mapping);
+    }
+
+    // 2. Prepare for next iteration:
+    // Map the iter_args to the values yielded at the end of THIS iteration.
+    auto yieldOp = cast<scf::YieldOp>(forOp.getBody()->getTerminator());
+    for (auto [arg, yieldVal] :
+        llvm::zip(forOp.getRegionIterArgs(), yieldOp.getOperands())) {
+      // We look up the yielded value in the mapper because it might have
+      // been newly created/cloned in this iteration.
+      mapping.map(arg, mapping.lookupOrDefault(yieldVal));
     }
   }
 
-  forOp.erase();
+  // 3. Replace loop results with the final mapped values of the iter_args
+  SmallVector<Value> finalValues;
+  for (Value arg : forOp.getRegionIterArgs()) {
+    finalValues.push_back(mapping.lookup(arg));
+  }
+
+  // This step prevents the "destroyed but still has uses" crash
+  rewriter.replaceOp(forOp, finalValues);
+
   return success();
 }
 
@@ -68,11 +79,72 @@ namespace marid {
 
 namespace {
 
+  static LogicalResult expandForLoop(scf::ForOp forOp, RewriterBase &rewriter) {
+    auto lbOpt = getConstantIndex(forOp.getLowerBound());
+    auto ubOpt = getConstantIndex(forOp.getUpperBound());
+    auto stepOpt = getConstantIndex(forOp.getStep());
+
+    if (!lbOpt || !ubOpt || !stepOpt || *stepOpt <= 0)
+      return failure();
+
+    int64_t lb = *lbOpt;
+    int64_t ub = *ubOpt;
+    int64_t step = *stepOpt;
+    int64_t tripCount = (ub - lb + step - 1) / step;
+
+    Location loc = forOp.getLoc();
+    IRMapping mapping;
+
+    // 1. Initialize mapping for iter_args
+    // The initial values of the loop's block arguments come from the init_args
+    for (auto [arg, initVal] :
+        llvm::zip(forOp.getRegionIterArgs(), forOp.getInits())) {
+      mapping.map(arg, initVal);
+    }
+
+    rewriter.setInsertionPoint(forOp);
+
+    for (int64_t i = 0; i < tripCount; ++i) {
+      // Update the Induction Variable for this iteration
+      Value ivValue =
+        arith::ConstantIndexOp::create(rewriter, loc, lb + i * step);
+      mapping.map(forOp.getInductionVar(), ivValue);
+
+      // Clone the body
+      for (Operation &op : forOp.getBody()->without_terminator()) {
+        rewriter.clone(op, mapping);
+      }
+
+      // 2. Prepare for next iteration: 
+      // Map the iter_args to the values yielded at the end of THIS iteration.
+      auto yieldOp = cast<scf::YieldOp>(forOp.getBody()->getTerminator());
+      for (auto [arg, yieldVal] :
+          llvm::zip(forOp.getRegionIterArgs(), yieldOp.getOperands())) {
+        // We look up the yielded value in the mapper because it might have 
+        // been newly created/cloned in this iteration.
+        mapping.map(arg, mapping.lookupOrDefault(yieldVal));
+      }
+    }
+
+    // 3. Replace loop results with the final mapped values of the iter_args
+    SmallVector<Value> finalValues;
+    for (Value arg : forOp.getRegionIterArgs()) {
+      finalValues.push_back(mapping.lookup(arg));
+    }
+
+    // This is the critical step that prevents the "destroyed but still has
+    // uses" crash
+    rewriter.replaceOp(forOp, finalValues);
+
+    return success();
+  }
+
 struct LoopExpansionPass
     : public PassWrapper<LoopExpansionPass, OperationPass<ModuleOp>> {
 
   void runOnOperation() override {
     ModuleOp module = getOperation();
+    IRRewriter rewriter(&getContext());
 
     // 1. Query ConstantBoundednessAnalysis
     auto &analysis = getAnalysis<ConstantBoundednessAnalysis>();
@@ -84,15 +156,13 @@ struct LoopExpansionPass
     }
 
     // 2. Expand loops innermost-first
-    SmallVector<scf::ForOp, 8> loops;
+    SmallVector<scf::ForOp> loops;
     module.walk<WalkOrder::PostOrder>([&](scf::ForOp forOp) {
-      loops.push_back(forOp);
-    });
+        loops.push_back(forOp);
+        });
 
     for (scf::ForOp forOp : loops) {
-      if (failed(expandForLoop(forOp))) {
-        forOp.emitError()
-            << "failed to expand scf.for (expected constant bounds)";
+      if (failed(expandForLoop(forOp, rewriter))) { // Pass rewriter here
         signalPassFailure();
         return;
       }
